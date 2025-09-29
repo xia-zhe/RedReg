2a75757eb02074322c7f79104fc49bd1933fbd52
X.zhe <139977177+xia-zhe@users.noreply.github.com>
2025-09-29 18:15:41 +0800

    Add files via upload

---
+++ models/basic_model.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .backbone import resnet18
+from .fusion_modules import SumFusion, ConcatFusion, FiLM, GatedFusion
+
+
+class AVClassifier(nn.Module):
+    def __init__(self, args):
+        super(AVClassifier, self).__init__()
+
+        fusion = args.fusion_method
+        if args.dataset == 'VGGSound':
+            n_classes = 309
+        elif args.dataset == 'KineticSound':
+            n_classes = 31
+        elif args.dataset == 'CREMAD':
+            n_classes = 6
+        elif args.dataset == 'AVE':
+            n_classes = 28
+        else:
+            raise NotImplementedError('Incorrect dataset name {}'.format(args.dataset))
+
+        if fusion == 'sum':
+            self.fusion_module = SumFusion(output_dim=n_classes)
+        elif fusion == 'concat':
+            self.fusion_module = ConcatFusion(output_dim=n_classes)
+        elif fusion == 'film':
+            self.fusion_module = FiLM(output_dim=n_classes, x_film=True)
+        elif fusion == 'gated':
+            self.fusion_module = GatedFusion(output_dim=n_classes, x_gate=True)
+        else:
+            raise NotImplementedError('Incorrect fusion method: {}!'.format(fusion))
+
+        self.audio_net = resnet18(modality='audio')
+        self.visual_net = resnet18(modality='visual')
+        self.head = nn.Linear(1024, n_classes)
+        self.head_audio = nn.Linear(512, n_classes)
+        self.head_video = nn.Linear(512, n_classes)
+
+    def forward(self, audio, visual):
+
+        a = self.audio_net(audio)
+        v = self.visual_net(visual)
+
+        (_, C, H, W) = v.size()
+        B = a.size()[0]
+        v = v.view(B, -1, C, H, W)
+        v = v.permute(0, 2, 1, 3, 4)
+
+        a = F.adaptive_avg_pool2d(a, 1)
+        v = F.adaptive_avg_pool3d(v, 1)
+
+        a = torch.flatten(a, 1)
+        v = torch.flatten(v, 1)
+
+        out = torch.cat((a,v),1)
+        out = self.head(out)
+
+        out_audio=self.head_audio(a)
+        out_video=self.head_video(v)
+
+
+        return audio, visual, a,v,out_audio,out_video,out
